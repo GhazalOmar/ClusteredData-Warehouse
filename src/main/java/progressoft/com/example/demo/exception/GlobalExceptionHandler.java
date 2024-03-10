59b2575724ec2f3095e1a4799c2f5c901f36521c
Ghazal Omar <ghazalomar02@gmail.com>
2024-03-10 11:22:18 +0300

    final commit

--- src/main/java/progressoft/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/progressoft/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package progressoft.com.example.demo.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -15,11 +16,13 @@ import java.util.List;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler({DealNotFoundException.class})
     public ResponseEntity<ResponseError> handleDealNotFoundException(DealNotFoundException exception) {
+        log.error("there is failure with deal", exception);
         ResponseError response = new ResponseError();
         response.setErrorCode(BAD_REQUEST.value());
         response.setErrorDescription(exception.getMessage());
@@ -29,6 +32,7 @@ public class GlobalExceptionHandler {
     @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler({DuplicateDealException.class})
     public ResponseEntity<ResponseError> handleDuplicateDealException(DuplicateDealException exception) {
+        log.error("there is failure with deal", exception);
         ResponseError response = new ResponseError();
         response.setErrorCode(BAD_REQUEST.value());
         response.setErrorDescription(exception.getMessage());
@@ -38,6 +42,7 @@ public class GlobalExceptionHandler {
     @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler({InvalidCurrencyException.class})
     public ResponseEntity<ResponseError> handleInvalidCurrencyException(InvalidCurrencyException exception) {
+        log.error("there is failure with deal", exception);
         ResponseError response = new ResponseError();
         response.setErrorCode(BAD_REQUEST.value());
         response.setErrorDescription(exception.getMessage());
@@ -47,13 +52,13 @@ public class GlobalExceptionHandler {
     @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ResponseError> methodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        log.error("there is failure with request fields", exception);
         BindingResult result = exception.getBindingResult();
         ResponseError response = new ResponseError();
         response.setErrorCode(BAD_REQUEST.value());
         response.setErrorDescription(getErrorDescription(result.getFieldErrors()));
         return ResponseEntity.badRequest().body(response);
     }
-
     private String getErrorDescription(List<FieldError> fieldErrors) {
         List<String> errors = new ArrayList<>();
         for (FieldError fieldError : fieldErrors) {
